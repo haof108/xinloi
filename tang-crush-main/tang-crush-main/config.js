80f4483dfebc8e6846e88982e61076560e29b5fe
haof108 <117980797+haof108@users.noreply.github.com>
2024-05-26 23:53:48 +0700

    Add files via upload

---
+++ tang-crush-main/tang-crush-main/config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Đan Lê à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Đan Lê tha lỗi tớ nhé',
+    desc: '',
+    btnYes: 'oke iu chồng',
+    btnNo: 'Không',
+    question:'hihi tớ biết mà',
+    btnReply: 'Gửi cho tớ ',
+    reply: 'Tớ yêu cậu',
+    mess: 'Tớ biết mà 🥰. Yêu vợ nhiều nhiều 😘😘',
+    messDesc: '3/6 gặp nhau nhé',
+    btnAccept: 'oke chồng iu',
+    messLink: 'https://www.facebook.com/haof08' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
